b4f22b3dd7599fbac36bc141fa80034d91627f1a
Melis Gökalp <40114428+melisgokalp@users.noreply.github.com>
2019-08-06 14:56:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import tweepy
 import os
 import time
 
-consumer_key = 'BivcUFjhHLUqJh2wHx7vTbhqM'
+consumer_key = ""
 # consumer_token = ''
-access_token = '159033588-ooqeO1LuZ1SL4qXUqGnTVs7mkClvqZJz66T4cWii'
-access_secret = 'gP84nBtHPo3HiDjPsN7BR6o0pzHVpSyjjs57gjbQCNUie'
-consumer_secret = "2l4FlvW7ftgTbq6a4ldgeWgR8SqJ82UyiLINyXGZXFsUPHVCz1"
+access_token = ""
+access_secret = ""
+consumer_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)    
 auth.set_access_token(access_token, access_secret)
